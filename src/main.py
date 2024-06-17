a87296b655f5529dc938877c1eebd835e13fc817
Dreamwave7 <dima190179@gmail.com>
2024-06-17 23:10:09 +0300

    commit

--- src/main.py
+++ src/main.py
@@ -11,16 +11,9 @@ app.include_router(user_router)
 
 @app.get("/", response_class=HTMLResponse)
 async def home(response : Response, db = Depends(get_session)):
-    response.set_cookie("testkey","testvalue")
     return """
     <a href="http://127.0.0.1:8000/docs">Documentation</a><br>
     <a href="http://127.0.0.1:8000/redoc">ReDoc</a>
     """
-
-
-@app.get("/cookie")
-async def get_cookie(request: Request):
-    token = request.cookies.get("testkey")
-    return token
 
     
--- src/users/router.py
+++ src/users/router.py
@@ -11,33 +11,44 @@ async def register(user_data : UserRegister):
     result = await UserService.register_new_user(user_data)
     return result 
 
-# @user_router.post("/login")
-# async def login(response:Response, user_data = Depends(OAuth2PasswordRequestForm)):
-#     checker_user = await AuthService.check_user(user_data.username, user_data.password)
-#     if not checker_user:
-#         raise InvalidLoginExceptions
-#     access_token = await AuthService.create_access_token(user_data.username)
-
-#     response.set_cookie("access_token", access_token, max_age=10000)
-#     return access_token
-
-
 @user_router.post("/login")
-async def login(response:Response, email:str, password:str):
-    checker_user = await AuthService.check_user(email, password)
+async def login(response:Response, user_data = Depends(OAuth2PasswordRequestForm)):
+    checker_user = await AuthService.check_user(user_data.username, user_data.password)
     if not checker_user:
         raise InvalidLoginExceptions
-    access_token = await AuthService.create_access_token(email)
+    access_token = await AuthService.create_access_token(user_data.username)
 
     response.set_cookie("access_token", access_token, max_age=10000)
     return access_token
 
 
+@user_router.get("/delete")
+async def delete_user(request: Request):
+    pass
 
 
-@user_router.get("/check_cookie")
+@user_router.get("/logout")
+async def logout_user(response:Response):
+    response.delete_cookie("access_token")
+    return "User logged out"
+
+
+# @user_router.get("/login")
+# async def login(response:Response, email:str, password:str):
+#     checker_user = await AuthService.check_user(email, password)
+#     if not checker_user:
+#         raise InvalidLoginExceptions
+#     access_token = await AuthService.create_access_token(email)
+
+#     response.set_cookie("access_token", access_token, max_age=260000)
+#     return access_token
+
+
+
+
+@user_router.get("/who_iam")
 async def check_cookie(request:Request):
-    token = request.get("access_token")
-    print(token)
-
-    return token
+    token = request.cookies.get("access_token")
+    if token:
+        return token
+    return "User not logged"
